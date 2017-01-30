2b96d7399a5b655f0c749a6f4b803eb46a3bfe8e
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2017-01-31 02:14:38 +0600

    Limit cache time

--- src/main/java/com/bazlur/meetup/extended/MeetupApplication.java
+++ src/main/java/com/bazlur/meetup/extended/MeetupApplication.java
@@ -2,7 +2,14 @@ package com.bazlur.meetup.extended;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +17,17 @@ public class MeetupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetupApplication.class, args);
+	}
+
+	@Bean
+	public JCacheManagerCustomizer cacheManagerCustomizer() {
+		return cm -> cm.createCache("meetup.schedules", initConfiguration(Duration.TEN_MINUTES));
+	}
+
+	private MutableConfiguration<Object, Object> initConfiguration(Duration duration) {
+		return new MutableConfiguration<>()
+			.setStoreByValue(false)
+			.setStatisticsEnabled(true)
+			.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
 	}
 }
