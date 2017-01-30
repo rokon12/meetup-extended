1532783223e5ba9c91005b5351dcdb0d34a9d921
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2017-01-31 02:42:14 +0600

    Add proposal submission

---
+++ src/main/java/com/bazlur/meetup/extended/domain/Submission.java
@@ -0,0 +1,42 @@
+package com.bazlur.meetup.extended.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author Bazlur Rahman Rokon
+ * @since 1/31/17.
+ */
+@Entity
+@Data
+public class Submission implements Serializable {
+
+	@GeneratedValue
+	@Id
+	private Long id;
+
+	@Enumerated
+	private Track track;
+
+	@Column
+	private String speakerName;
+
+	@Column
+	private String email;
+
+	@Column
+	private String title;
+
+	@Column
+	private SubmissionStatus status;
+
+	@Column
+	@Lob
+	private String summary;
+
+	public Submission() {
+		this.status = SubmissionStatus.DRAFT;
+	}
+}
---
+++ src/main/java/com/bazlur/meetup/extended/dto/SubmissionForm.java
@@ -0,0 +1,34 @@
+package com.bazlur.meetup.extended.dto;
+
+import com.bazlur.meetup.extended.domain.Track;
+import lombok.Data;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Bazlur Rahman Rokon
+ * @since 1/31/17.
+ */
+@Data
+public class SubmissionForm {
+	@NotNull
+	private Track track;
+
+	@NotEmpty(message = "Speaker Name should not be empty")
+	private String speakerName;
+
+	@Email
+	@NotEmpty(message = "Email should not be empty")
+	private String email;
+
+	@NotEmpty(message = "Title should not be empty")
+	@Size(max = 200)
+	private String title;
+
+	@NotEmpty(message = "Summary should not be empty")
+	@Size(max = 1000)
+	private String summary;
+}
